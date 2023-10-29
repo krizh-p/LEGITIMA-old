8b127ee47bac27f71b8277600b10038bfb05f1dc
krizh-p <kpurohit@gmu.edu>
2023-10-29 10:53:04 -0400

    added reporting

--- website/vuitton/home/urls.py
+++ website/vuitton/home/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("legitima", views.legitima, name="legitima"),
     path('process_token/', views.process_token, name='process_token'),
+    path('invalid_token/', views.invalid_token, name='invalid_token'),
+    path('report/', views.report, name='report'),
+    path('thank_you/', views.thank_you, name='thank_you'),
 ]
--- website/vuitton/home/views.py
+++ website/vuitton/home/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .models import Token
+from .encrypt import encrypt, decrypt
 
 # Create your views here.
 def index(request):
@@ -8,20 +10,30 @@ def index(request):
 def legitima(request):
     return render(request, "home/legitima.html")
 
+def loadToken(request, data):
+    return render(request, "home/token.html", {
+        "data" : data
+    })
+
+def report(request):
+    return render(request, "home/report.html")
+
+def invalid_token(request):
+    return render(request, "home/invalid_token.html")
+
+def thank_you(request):
+    return render(request, "home/thank_you.html")
+
 def process_token(request):
     if request.method == 'POST':
         # Get the submitted token from the form
         token = request.POST.get('token', '')
-
-        # Here, you can process the 'token' as needed.
-        # For this example, we'll just print it to the console.
-        print(f"Received token: {token}")
-
-        # You can perform any further actions with the token here.
-
-        # After processing, you may want to redirect the user to a thank-you page or another page.
-        # Replace 'thank_you' with the actual URL pattern or path.
-        return HttpResponseRedirect('thank_you')
-
+        # Search for a Token with a matching primary key
+        try:
+            token_instance = Token.objects.get(pk=token)
+            return loadToken(request, token_instance)
+        except Token.DoesNotExist:
+            return render(request, "home/invalid_token.html")
+            
     # Handle GET requests or render the form again
     return render(request, 'legitima.html')
